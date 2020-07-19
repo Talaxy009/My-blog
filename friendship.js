41022883d77ea2534ec56439423f38708082e76f
Taozc <41264828+Talaxy009@users.noreply.github.com>
2020-07-19 21:27:28 +0800

    Change collections page

--- friendship.js
+++ friendship.js
@@ -4,5 +4,6 @@ const push = (name, url, image = "", description = "") =>
 
 push("WAI科技社", "https://waitech.cn/", "WAI.jpg");
 push("薪典", "https://syndict.com/", "syndict.png");
+push('路由器','http://snow-mountain.life:8080/', 'router.png');
 
 module.exports = friendship;
