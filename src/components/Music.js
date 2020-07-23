f817bc238037fc9a0ec07d30eeed95e0f40ba823
Taozc <41264828+Talaxy009@users.noreply.github.com>
2020-07-23 11:25:00 +0800

    Add transitions

--- src/components/Music.js
+++ src/components/Music.js
@@ -3,13 +3,14 @@ import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import Image from "gatsby-image";
+import Grow from '@material-ui/core/Grow';
 import CardContent from "@material-ui/core/CardContent";
-import MuiExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import MuiExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import MuiAccordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-const ExpansionPanel = withStyles({
+const Accordion = withStyles({
   root: {
     boxShadow: "none",
     "&:before": {
@@ -21,13 +22,13 @@ const ExpansionPanel = withStyles({
       margin: 0
     }
   }
-})(MuiExpansionPanel);
+})(MuiAccordion);
 
-const ExpansionPanelSummary = withStyles({
+const AccordionSummary = withStyles({
   root: {
     minHeight: "64px"
   }
-})(MuiExpansionPanelSummary);
+})(MuiAccordionSummary);
 
 const Music = ({ musicImages = [], props = [], data = [] }) => {
   return (
@@ -38,48 +39,52 @@ const Music = ({ musicImages = [], props = [], data = [] }) => {
         );
         return (
           <div className="music-card" key={music.name}>
-            <Card
-              style={{
-                transition: "background-color 0.3s ease",
-                color: "inherit"
-              }}
-            >
-              <Image
-                alt={props.alt}
-                fluid={image.childImageSharp.fluid}
-                style={{
-                  flex: 1,
-                  margin: "0.9rem auto",
-                  maxWidth: "90%",
-                  borderRadius: "100%"
-                }}
-                imgStyle={{
-                  borderRadius: "50%"
-                }}
-              />
-              <CardContent>
-                <Typography variant="body1" component="p">
-                  专辑：{music.name}
-                </Typography>
-                <Typography variant="body1" component="p">
-                  乐队：{music.band}
-                </Typography>
-                <Typography variant="body1" component="p">
-                  发行日期：{music.time}
-                </Typography>
-              </CardContent>
-              <ExpansionPanel
-                style={{ backgroundColor: "inherit", color: "inherit" }}
-              >
-                <ExpansionPanelSummary
-                  expandIcon={<ExpandMoreIcon style={{ color: "#8f8f8f" }} />}
-                  id={music.image}
-                ></ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                  <Typography>{music.description}</Typography>
-                </ExpansionPanelDetails>
-              </ExpansionPanel>
-            </Card>
+            <Grow in={true} {...{ timeout: 500 }}>
+              <div>
+                <Card
+                  style={{
+                    transition: "background-color 0.3s ease",
+                    color: "inherit"
+                  }}
+                >
+                  <Image
+                    alt={props.alt}
+                    fluid={image.childImageSharp.fluid}
+                    style={{
+                      flex: 1,
+                      margin: "0.9rem auto",
+                      maxWidth: "90%",
+                      borderRadius: "100%"
+                    }}
+                    imgStyle={{
+                      borderRadius: "50%"
+                    }}
+                  />
+                  <CardContent>
+                    <Typography variant="body1" component="p">
+                      专辑：{music.name}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                      乐队：{music.band}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                      发行日期：{music.time}
+                    </Typography>
+                  </CardContent>
+                  <Accordion
+                    style={{ backgroundColor: "inherit", color: "inherit" }}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon style={{ color: "#8f8f8f" }} />}
+                      id={music.image}
+                    ></AccordionSummary>
+                    <AccordionDetails>
+                      <Typography>{music.description}</Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                </Card>
+              </div>
+            </Grow>
           </div>
         );
       })}
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react";
 import { Link } from "gatsby";
-
 import { rhythm } from "../utils/typography";
 import { formatReadingTime } from "../utils/helper";
+import Slide from '@material-ui/core/Slide';
 
 const PostList = ({ posts = [] }) => {
   return (
@@ -11,28 +11,32 @@ const PostList = ({ posts = [] }) => {
         const title = node.frontmatter.title || node.fields.slug;
         return (
           <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4)
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>
-                {node.frontmatter.date}
-                {` • ${formatReadingTime(node.timeToRead)}`}
-              </small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt
-                }}
-              />
-            </section>
+            <Slide in={true} direction={"up"} {...{ timeout: 500 }} mountOnEnter unmountOnExit>
+              <div>
+                <header>
+                  <h3
+                    style={{
+                      marginBottom: rhythm(1 / 4)
+                    }}
+                  >
+                    <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                      {title}
+                    </Link>
+                  </h3>
+                  <small>
+                    {node.frontmatter.date}
+                    {` • ${formatReadingTime(node.timeToRead)}`}
+                  </small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt
+                    }}
+                  />
+                </section>
+              </div>
+            </Slide>
           </article>
         );
       })}
