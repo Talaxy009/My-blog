881f8f1bc91aa231cac9004a7a87939c65a4530b
Taozc <41264828+Talaxy009@users.noreply.github.com>
2020-07-24 10:08:15 +0800

    Add dark mode button transition

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useDarkMode from "use-dark-mode";
+import Slide from "@material-ui/core/Slide"
 import Sun from "@material-ui/icons/WbSunnyRounded";
 import Moon from "@material-ui/icons/Brightness2Rounded";
 import IconButton from "@material-ui/core/IconButton";
@@ -12,11 +13,28 @@ const DarkMode = () => {
         type="button"
         onClick={darkMode.value ? darkMode.disable : darkMode.enable}
       >
-        {darkMode.value ? (
-          <Moon style={{ color: "#ffb74d" }} />
-        ) : (
-          <Sun style={{ color: "#ff9800" }} />
-        )}
+        <Slide
+          direction={darkMode.value ? "down" : "up"}
+          in={darkMode.value}
+          appear={false}
+          {...{ timeout: 500 }}
+          style={{ transitionDelay: darkMode.value ? '500ms' : '0ms' }}
+          mountOnEnter
+          unmountOnExit
+        >
+          <Moon style={{ position: "absolute", color: "#ffb74d" }} />
+        </Slide>
+        <Slide
+          direction={darkMode.value ? "up" : "down"}
+          in={!darkMode.value}
+          appear={false}
+          {...{ timeout: 500 }}
+          style={{ transitionDelay: darkMode.value ? '0ms' : '500ms' }}
+          mountOnEnter
+          unmountOnExit
+        >
+          <Sun style={{ position: "absolute", color: "#ff9800" }} />
+        </Slide>
       </IconButton>
     </div>
   );
