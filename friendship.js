9da2b15a5b15a84ecf1854df3cdec26990ae95f1
Taozc <41264828+Talaxy009@users.noreply.github.com>
2020-09-06 20:19:20 +0800

    Change the url of my router

--- friendship.js
+++ friendship.js
@@ -4,6 +4,6 @@ const push = (name, url, image = "", description = "") =>
 
 push("WAI科技社", "https://waitech.cn/", "WAI.jpg");
 push("薪典", "https://syndict.com/", "syndict.png");
-push("路由器", "http://snow-mountain.life:8080/", "router.png");
+push("路由器", "/router/", "router.png");
 
 module.exports = friendship;
